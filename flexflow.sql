71d20068bfb9376914ccd7286ba5f9a4b8ace097
Steve Larsen <stevelarsen01@gmail.com>
2017-01-05 10:10:44 -0600

    update .gitignore

--- flexflow.sql
+++ flexflow.sql
@@ -44,10 +44,78 @@ CREATE TABLE item (
 
 INSERT INTO item (item_name, category_id, item_sequence, item_text, item_description, item_input_mode) 
 VALUES ('Holidays', 2, 1, 'Holidays', 'Enter monthly holiday expenses.  For example, you may put $200 for Christmas in December', 'V'),
-('Birthdays', 2, 2, 'Birthdays', 'Enter monthly birthday expenses.  For example, you may put $200 for your partner in May, and $20 for Aunt Sally in July', 'V')
+('Birthdays', 2, 2, 'Birthdays', 'Enter monthly birthday expenses.  For example, you may put $200 for your partner in May, and $20 for Aunt Sally in July', 'V'),
+('Housing', 4, 1, 'Rent | Mortgage', 'Enter monthly expense for housing.', 'F')
+
 ;
 
 SELECT * FROM item;
+
+DROP TABLE budget;
+
+CREATE TABLE budget (
+    budget_id serial PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    budget_year integer
+);
+
+INSERT INTO budget (user_id, budget_year) 
+VALUES (1, 2017),
+(2, 2017)
+;
+
+SELECT * FROM budget;
+
+DROP TABLE budget;
+
+CREATE TABLE answer (
+    answer_id serial PRIMARY KEY,
+    budget_id INTEGER REFERENCES budget,
+    answer_name character varying(20),
+    answer_sequence integer,
+    answer_text character varying(255), 
+    value_monthly INTEGER,
+    value_01 INTEGER,
+    value_02 INTEGER,
+    value_03 INTEGER,
+    value_04 INTEGER,
+    value_05 INTEGER,
+    value_06 INTEGER,
+    value_07 INTEGER,
+    value_08 INTEGER,
+    value_09 INTEGER,
+    value_10 INTEGER,
+    value_11 INTEGER,
+    value_12 INTEGER,
+    value_yearly INTEGER
+);
+
+INSERT INTO answer (budget_id, answer_name,
+    answer_sequence,
+    answer_text, 
+    value_monthly,
+    value_01,
+    value_02,
+    value_03,
+    value_04,
+    value_05,
+    value_06,
+    value_07,
+    value_08,
+    value_09,
+    value_10,
+    value_11,
+    value_12,
+    value_yearly) 
+VALUES (1, 'Holidays', 1, 'Holidays', null, 0, 100, 0, 0, 100, 0, 100, 0, 0, 0, 100, 200, 600),
+(1, 'Birthdays', 2, 'Birthdays', null, 0, 0, 0, 0, 0, 200, 0, 0, 0, 200, 0, 0, 400),
+(1, 'Birthdays', 2, 'Birthdays', null, 0, 0, 0, 0, 0, 200, 0, 0, 0, 200, 0, 0, 400)
+;
+
+SELECT * FROM answer;
+
+
+
 
 
 
